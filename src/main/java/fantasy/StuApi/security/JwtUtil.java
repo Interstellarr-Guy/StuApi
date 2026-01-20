346d2ef6f082c6ba8c018c8cb56af3f0bca67131
Deva kumar <rajmechn@gmail.com>
2026-01-20 21:48:14 +0530

    auth changed

--- src/main/java/fantasy/StuApi/security/JwtUtil.java
+++ src/main/java/fantasy/StuApi/security/JwtUtil.java
@@ -2,6 +2,7 @@ package fantasy.StuApi.security;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Jwts;
@@ -12,22 +13,22 @@ import jakarta.annotation.PostConstruct;
 @Component
 public class JwtUtil {
 
-  private final String SECRET = "CHANGE_THIS_SECRET_KEY_1234567891";
+  @Value("${jwt.secret}")
+  private String secret;
 
   public String generateToken(String username) {
     return Jwts.builder()
         .setSubject(username)
         .setIssuedAt(new Date())
         .setExpiration(new Date(System.currentTimeMillis() + 86400000))
-        .signWith(Keys.hmacShaKeyFor(SECRET.getBytes()), SignatureAlgorithm.HS256)
+        .signWith(Keys.hmacShaKeyFor(secret.getBytes()), SignatureAlgorithm.HS256)
         .compact();
   }
-  
+
   @PostConstruct
   public void checkSecret() {
-    if (SECRET == null || SECRET.length() < 32) {
+    if (secret == null || secret.length() < 32) {
       throw new IllegalStateException("JWT secret is missing or too short");
     }
   }
-
 }
