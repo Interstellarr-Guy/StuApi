51052ae7347585839b19d6746eb93ffa99086d4a
Deva kumar <rajmechn@gmail.com>
2026-01-30 15:12:48 +0530

    typo changes

--- src/main/java/fantasy/StuApi/security/JwtUtil.java
+++ src/main/java/fantasy/StuApi/security/JwtUtil.java
@@ -19,7 +19,7 @@ public class JwtUtil {
   public String generateToken(String username, String role) {
     return Jwts.builder()
         .setSubject(username)
-        .claim("roles","ROLE_" + role) //2301
+        .claim("roles",role) //2301
         .setIssuedAt(new Date())
         .setExpiration(new Date(System.currentTimeMillis() + 86400000))
         .signWith(Keys.hmacShaKeyFor(secret.getBytes()), SignatureAlgorithm.HS256)
